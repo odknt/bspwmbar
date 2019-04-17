e7e10d54970cc5dda3ed0d9720faeb95618785f7
odknt <4185305+odknt@users.noreply.github.com>
2019-04-17 11:16:48 +0900

    fix config.mk

--- config.mk
+++ config.mk
@@ -1,11 +1,11 @@
-VERSION = 0
+VERSION ?= 0
 
-PREFIX    = /usr/local
-DESTDIR   =
-MANPREFIX = $(PREFIX)/share/man
+PREFIX    ?= /usr/local
+DESTDIR   ?=
+MANPREFIX ?= $(PREFIX)/share/man
 
 CFLAGS  += -std=c99 -pedantic -Wall -Wextra -I/usr/include/freetype2
 LDFLAGS +=
 LDLIBS  += -lX11 -lfontconfig -lXft -lXrandr -lasound
 
-CC = cc
+CC ?= cc
